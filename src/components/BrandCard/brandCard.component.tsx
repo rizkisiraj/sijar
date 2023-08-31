1e9eade48e09c902cb540aa9ac21234f0ec6e607
rizkisiraj <jomirwa@gmail.com>
2023-08-31 22:26:05 +0700

    initial commit

---
+++ src/components/BrandCard/brandCard.component.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from "react";
+
+interface Brand {
+    icon: ReactElement
+    name: string
+    link: string
+}
+
+const BrandCard = (brand: Brand) => {
+    return (
+        <a className={`inline-flex gap-1 items-center py-[2px] px-1 border border-gray-500 rounded-md bg-gray-600 ${brand.name === "Flutter" ? "text-red-300" : (brand.name === "SwiftUI" ? "text-blue-300" : "text-green-300")}`} href={brand.link}>{brand.icon} {brand.name}</a>
+    )
+}
+
+export default BrandCard
