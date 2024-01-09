92fddb21829de22c56707c07dd66ea10ed191eee
rizkisiraj <jomirwa@gmail.com>
2024-01-09 08:03:24 +0700

    fix: several bug fix

--- src/helpers/convertDate.ts
+++ src/helpers/convertDate.ts
@@ -38,31 +38,5 @@ function formatDateWithAgo(date: Date) {
 
   return formattedDate + " " + diff;
 }
-    const months = [
-      'January', 'February', 'March', 'April',
-      'May', 'June', 'July', 'August',
-      'September', 'October', 'November', 'December'
-    ];
-  
-    const currentDate = new Date();
-    const targetDate: Date = new Date(date);
-    
-    const yearDiff = currentDate.getFullYear() - targetDate.getFullYear();
-    const monthDiff = currentDate.getMonth() - targetDate.getMonth();
-    
-  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-   const formattedDate = `${months[targetDate.getMonth()]} ${targetDate.getDate()}, ${targetDate.getFullYear()}`;
-    let diff: string;
-    
-    if (yearDiff === 0 && monthDiff === 0) {
-      diff = " (this month)";
-    } else if (yearDiff === 0) {
-      diff = ` (${monthDiff}mo ago)`;
-    } else {
-      diff = ` (${yearDiff}y ${monthDiff}mo ago)`;
-    }
-    
-    return formattedDate + " " + diff;
-  }
 
   export default formatDateWithAgo;
