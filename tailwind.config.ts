e6ad98c5229b28179e0f03423f4bdd22f18eea8b
rizkisiraj <jomirwa@gmail.com>
2024-01-21 20:35:38 +0700

    feat: add moviedb projects

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,22 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      'sm': '400px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       fontFamily: {
         'sans': ['Poppins', 'sans-serif']
