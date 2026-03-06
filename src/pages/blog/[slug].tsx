4200b315ddd3971fbea9b08702a66bd75b096196
rizki siraj <jomirwa@gmail.com>
2026-03-07 00:05:41 +0700

    feat: add new post

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -30,7 +30,7 @@ export default function Post(props:Props) {
       <time className="text-gray-500 text-sm">{formatDateWithAgo(new Date(data.date))}</time>
         <p className="text-gray-500 text-sm">2,000 Views</p>
       </div>
-      <div className="prose-lg prose-code:bg-inherit prose-h2:font-semibold prose-h2:text-2xl prose-h2:text-neutral-300 prose-code:text-neutral-300 prose-pre:bg-gray-700 prose-p:text-neutral-300 mt-12 prose-code:text-sm prose-pre:max-w-full prose-pre:overflow-x-auto">
+      <div className="prose-lg prose-code:bg-inherit prose-h2:font-semibold prose-h2:text-2xl prose-h2:text-neutral-300 prose-code:text-neutral-300 prose-pre:bg-gray-700 prose-p:text-neutral-300 mt-12 prose-code:text-sm prose-pre:max-w-full prose-pre:overflow-x-auto prose-li:text-neutral-300 prose-ul:list-disc">
         <MDXRemote {...content} components={{ Image }} />
       </div>
     </div>
